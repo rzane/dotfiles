87966c71fda3bb0e49a26a256befe2e53c0e4fa0
Ray Zane <ray@promptworks.com>
2017-08-14 11:55:57 -0400

    Load rvm

--- shell/init.sh
+++ shell/init.sh
@@ -5,5 +5,8 @@ bindkey '^[[B' history-substring-search-down
 # Set project paths for project jump
 PROJECT_PATHS=(~/Workspace)
 
-# Load Kiex for elixir
+# Load Kiex for Elixir
 test -s "$HOME/.kiex/scripts/kiex" && source "$HOME/.kiex/scripts/kiex"
+
+# Load RVM for Ruby
+test -s "$HOME/.rvm/scripts/rvm" && source "$HOME/.rvm/scripts/rvm"
