2c2b80a93d84f7d49fa5fdd1c43871433ef3a41e
Ray Zane <raymondzane@gmail.com>
2022-12-05 02:08:13 -0500

    Make bufferline look good

--- nvim/lua/bufferline-config.lua
+++ nvim/lua/bufferline-config.lua
@@ -1,16 +1,32 @@
 local colors = require('material.colors')
-local fg = colors.editor.fg
-local bg = colors.editor.contrast
-local hint = colors.lsp.hint
-local info = colors.lsp.info
-local error = colors.main.darkred
-local warning = colors.lsp.warning
-local modified = colors.git.modified
 local border = colors.editor.border
+
+local bg = {
+  default = colors.editor.bg,
+  tab =  colors.editor.contrast,
+  active = colors.editor.active
+}
+
+local text = {
+  default = colors.editor.fg,
+  hint = colors.lsp.hint,
+  info = colors.lsp.info,
+  error = colors.main.darkred,
+  warning = colors.lsp.warning,
+  modified = colors.git.modified,
+  numbers = colors.editor.gray
+}
+
+local style = function(fg)
+  return { fg = fg, bg = bg.tab }
+end
+
+local active_style = function(fg)
+  return { fg = fg, bg = bg.active, bold = false, italic = false }
+end
 
 return {
   options = {
-    -- numbers = "ordinal",
     show_close_icon = false,
     show_buffer_icons = false,
     diagnostics = "nvim_lsp",
@@ -19,27 +35,55 @@ return {
     end,
   },
   highlights = {
-    fill = { bg = bg, fg = fg },
-    background = { bg = bg, fg = fg },
-    tab = { bg = bg, fg = fg },
-    close_button = { bg = bg },
-    numbers = { bg = bg, fg = fg },
-    separator = { bg = bg, fg = border },
-    separator_selected = { bg = colors.editor.bg, fg = border },
-    pick = { bg = bg },
-    offset_separator = { bg = colors.editor.bg },
-    hint = { fg = hint },
-    hint_selected = { bold = false, italic = false, fg = hint },
-    info = { fg = info },
-    info_selected = { bold = false, italic = false, fg = info },
-    modified = { fg = modified },
-    modified_selected = { bold = false, italic = false, fg = modified },
-    warning = { fg = warning },
-    warning_selected = { bold = false, italic = false, fg = warning },
-    error = { fg = error },
-    error_selected = { bold = false, italic = false, fg = error },
-    buffer_selected = { bold = false, italic = false, bg = colors.editor.bg },
-    numbers_selected = { bold = false, italic = false },
-    diagnostic_selected = { bold = false, italic = false },
+    fill = { bg = colors.editor.bg },
+    background = style(text.default),
+    tab = style(text.default),
+    tab_selected = active_style(text.default),
+    tab_close = style(text.default),
+    close_button = style(text.default),
+    close_button_visible = style(text.default),
+    close_button_selected = active_style(text.default),
+    buffer_visible = style(text.default),
+    buffer_selected = active_style(text.default),
+    numbers = style(text.numbers),
+    numbers_visible = style(text.numbers),
+    numbers_selected = active_style(text.numbers),
+    diagnostic = style(text.default),
+    diagnostic_visible = style(text.default),
+    diagnostic_selected = active_style(text.default),
+    hint = style(text.hint),
+    hint_visible = style(text.hint),
+    hint_selected = active_style(text.hint),
+    hint_diagnostic = style(text.hint),
+    hint_diagnostic_visible = style(text.hint),
+    hint_diagnostic_selected = active_style(text.hint),
+    info = style(text.info),
+    info_visible = style(text.info),
+    info_selected = active_style(text.info),
+    info_diagnostic = style(text.info),
+    info_diagnostic_visible = style(text.info),
+    info_diagnostic_selected = active_style(text.info),
+    warning = style(text.warning),
+    warning_visible = style(text.warning),
+    warning_selected = active_style(text.warning),
+    warning_diagnostic = style(text.warning),
+    warning_diagnostic_visible = style(text.warning),
+    warning_diagnostic_selected = active_style(text.warning),
+    error = style(text.error),
+    error_visible = style(text.error),
+    error_selected = active_style(text.error),
+    error_diagnostic = style(text.error),
+    error_diagnostic_visible = style(text.error),
+    error_diagnostic_selected = active_style(text.error),
+    modified = style(text.modified),
+    modified_visible = style(text.modified),
+    modified_selected = active_style(text.modified),
+    duplicate = style(text.modified),
+    duplicate_selected = active_style(text.modified),
+    duplicate_visible = style(text.modified),
+    separator = { fg = border, bg = bg.tab },
+    separator_selected = { fg = border, bg = bg.active },
+    separator_visible = { fg = border, bg = bg.tab },
+    indicator_selected = { fg = bg.active, bg = bg.active },
   }
 }
