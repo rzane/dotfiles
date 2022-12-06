2b221ed41a99fc2d0ac34d0e635b82296f7a6983
Ray Zane <raymondzane@gmail.com>
2022-12-06 11:30:58 -0500

    Autoclose quits vim after selecting a file with telescope

--- nvim/init.lua
+++ nvim/init.lua
@@ -77,16 +77,6 @@ require("nvim-tree").setup(require("nvim-tree-config"))
 vim.keymap.set("n", "<leader>ee", ":NvimTreeToggle<CR>", { desc = "toggle file explorer" })
 vim.keymap.set("n", "<leader>ef", ":NvimTreeFocus<CR>", { desc = "focus file explorer" })
 
--- Quit vim when nvim-tree is the last window
-vim.api.nvim_create_autocmd("BufEnter", {
-  nested = true,
-  callback = function()
-    if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
-      vim.cmd "quit"
-    end
-  end
-})
-
 --------------------------
 -- Buffers
 --------------------------
