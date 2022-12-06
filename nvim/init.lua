6b0dea4754f69f62cac38e69b27af5bb15b5284c
Ray Zane <raymondzane@gmail.com>
2022-12-05 22:38:38 -0500

    Autoclose

--- nvim/init.lua
+++ nvim/init.lua
@@ -69,6 +69,16 @@ require("nvim-tree").setup(require("nvim-tree-config"))
 
 vim.keymap.set("n", "<Leader>t", ":NvimTreeToggle<CR>")
 
+-- Quit vim when nvim-tree is the last window
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
+      vim.cmd "quit"
+    end
+  end
+})
+
 --------------------------
 -- Buffers
 --------------------------
