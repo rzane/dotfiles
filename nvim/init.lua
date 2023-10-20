8927b83cafeaff9bfefc10c3e76a4ebcc8b12a05
Ray Zane <raymondzane@gmail.com>
2023-10-20 16:46:33 -0400

    Configure CMP mappings

--- nvim/init.lua
+++ nvim/init.lua
@@ -219,6 +219,17 @@ require('mason-lspconfig').setup({
   }
 })
 
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = {
+    ['<Tab>'] = cmp_action.tab_complete(),
+    ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+  }
+})
+
 -----------------------
 -- Terminal
 -----------------------
